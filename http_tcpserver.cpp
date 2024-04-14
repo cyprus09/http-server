7218b7a1760e1884e49e24dedb34e12ebccd7d3c
mayankpallai <mayankpallai@gmail.com>
2024-04-14 23:27:34 +0800

    create close and naming socket functions

--- http_tcpserver.cpp
+++ http_tcpserver.cpp
@@ -1,11 +1,53 @@
-#include <http_tcpServer.h>
+#include "http_tcpServer.h"
 
-namespace http {
-    TcpServer :: TcpServer(){
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
 
+namespace
+{
+    void log(const std::string &message)
+    {
+        std::cout << message << std::endl;
     }
 
-    TcpServer::~TcpServer(){
-        
+    void exitWithError(const std::string &errorMessage)
+    {
+        log("Error: " + errorMessage);
+        exit(1);
     }
+
+}
+namespace http
+{
+
+    TcpServer ::TcpServer(std::string ip_address, int port) : m_ip_address(ip_address), m_port(port), m_socket(), m_new_socket(), m_incoming_message(), m_socket_address(), m_socket_address_len(sizeof(m_socket_address)), m_server_message(buildRespone())
+    {
+        startServer();
+    }
+
+    TcpServer::~TcpServer()
+    {
+        closeServer();
+    }
+
+    int TcpServer::startServer()
+    {
+        m_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (m_socket < 0)
+        {
+            exitWithError("Cannot create socket");
+            return 1;
+        }
+
+        return 0;
+    }
+
+    void TcpServer::closeServer()
+    {
+        close(m_socket);
+        close(m_new_socket);
+        exit(0);
+    }
+
 }
--- http_tcpserver.h
+++ http_tcpserver.h
@@ -1,16 +1,33 @@
 #ifndef INCLUDED_HTTP_TCPSERVER
 #define INCLUDED_HTTP_TCPSERVER
 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string>
+
 namespace http
 {
     class TcpServer
     {
     public:
-        TcpServer();
+        TcpServer(std::string ip_address, int port);
         ~TcpServer();
 
     private:
-    
+        std::string m_ip_address;
+        int m_port;
+        int m_socket;
+        int m_new_socket;
+        int m_incoming_message;
+        struct sockaddr_in m_socket_address;
+        unsigned int m_socket_address_len;
+        std::string m_server_message;
+
+        int startServer();
+        void closeServer();
+        std::string buildResponse();
     };
 }
 #endif
